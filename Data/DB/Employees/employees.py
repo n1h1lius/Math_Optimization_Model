431d3cbd1dc077db6f70f349ed0bd97a705b21de
Nihilus Null Zero <135299351+n1h1lius@users.noreply.github.com>
2025-03-14 04:05:39 +0100

    Add files via upload

---
+++ Data/DB/Employees/employees.py
@@ -0,0 +1,820 @@
+
+worker_0 = {
+	"id": "0",
+	"alias": "Aristotle",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_1 = {
+	"id": "1",
+	"alias": "Nietzsche",
+	"role": "3",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_2 = {
+	"id": "2",
+	"alias": "Herman_H",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_3 = {
+	"id": "3",
+	"alias": "Baldr",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_4 = {
+	"id": "4",
+	"alias": "Anton",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_5 = {
+	"id": "5",
+	"alias": "Philip_J",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_6 = {
+	"id": "6",
+	"alias": "Carl_J",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_7 = {
+	"id": "7",
+	"alias": "Albert_E",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_8 = {
+	"id": "8",
+	"alias": "Frank_L",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_9 = {
+	"id": "9",
+	"alias": "Viktor",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_10 = {
+	"id": "10",
+	"alias": "Joe_R",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_11 = {
+	"id": "11",
+	"alias": "Francis",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_12 = {
+	"id": "12",
+	"alias": "Louise",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_13 = {
+	"id": "13",
+	"alias": "Ferdinand",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_14 = {
+	"id": "14",
+	"alias": "Robert_P",
+	"role": "3",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_15 = {
+	"id": "15",
+	"alias": "Samantha_H",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_16 = {
+	"id": "16",
+	"alias": "Alex_B",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_17 = {
+	"id": "17",
+	"alias": "Tom_H",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_18 = {
+	"id": "18",
+	"alias": "Christopher",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_19 = {
+	"id": "19",
+	"alias": "Oleg",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_20 = {
+	"id": "20",
+	"alias": "Chris_T",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_21 = {
+	"id": "21",
+	"alias": "Demian",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_22 = {
+	"id": "22",
+	"alias": "Kromer",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_23 = {
+	"id": "23",
+	"alias": "Harry_H",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_24 = {
+	"id": "24",
+	"alias": "Haller_N",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_25 = {
+	"id": "25",
+	"alias": "Daniel",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_26 = {
+	"id": "26",
+	"alias": "David",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_27 = {
+	"id": "27",
+	"alias": "Elisse",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_28 = {
+	"id": "28",
+	"alias": "Elisabeth",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_29 = {
+	"id": "29",
+	"alias": "Frederic",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_30 = {
+	"id": "30",
+	"alias": "Paul_H",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_31 = {
+	"id": "31",
+	"alias": "John_L",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_32 = {
+	"id": "32",
+	"alias": "Helen",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_33 = {
+	"id": "33",
+	"alias": "Artyom",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_34 = {
+	"id": "34",
+	"alias": "Richard_B",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_35 = {
+	"id": "35",
+	"alias": "Gabriel",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_36 = {
+	"id": "36",
+	"alias": "Steve_S",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_37 = {
+	"id": "37",
+	"alias": "Rick_G",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_38 = {
+	"id": "38",
+	"alias": "Connor",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_39 = {
+	"id": "39",
+	"alias": "Gregor",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_40 = {
+	"id": "40",
+	"alias": "John_G",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_41 = {
+	"id": "41",
+	"alias": "Robert_K",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_42 = {
+	"id": "42",
+	"alias": "Martin_P",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_43 = {
+	"id": "43",
+	"alias": "Stephanie",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_44 = {
+	"id": "44",
+	"alias": "Claire",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_45 = {
+	"id": "45",
+	"alias": "Bob_M",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_46 = {
+	"id": "46",
+	"alias": "Tom_D",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_47 = {
+	"id": "47",
+	"alias": "Joe_M",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_48 = {
+	"id": "48",
+	"alias": "John_B",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_49 = {
+	"id": "49",
+	"alias": "Geralt_R",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_50 = {
+	"id": "50",
+	"alias": "Joe_S",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_51 = {
+	"id": "51",
+	"alias": "Mary_M",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_52 = {
+	"id": "52",
+	"alias": "Mary_G",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_53 = {
+	"id": "53",
+	"alias": "Martha_L",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_54 = {
+	"id": "54",
+	"alias": "Ruth_T",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_55 = {
+	"id": "55",
+	"alias": "Andrew_F",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_56 = {
+	"id": "56",
+	"alias": "Keanu_R",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_57 = {
+	"id": "57",
+	"alias": "Gregory",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_58 = {
+	"id": "58",
+	"alias": "Sheldon_C",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_59 = {
+	"id": "59",
+	"alias": "Leonard_H",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_60 = {
+	"id": "60",
+	"alias": "Rebecca_G",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_61 = {
+	"id": "61",
+	"alias": "Danielle_G",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_62 = {
+	"id": "62",
+	"alias": "Samuel_J",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_63 = {
+	"id": "63",
+	"alias": "Quentin_T",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_64 = {
+	"id": "64",
+	"alias": "George_B",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_65 = {
+	"id": "65",
+	"alias": "Mark_H",
+	"role": "2",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_66 = {
+	"id": "66",
+	"alias": "Gregor_T",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_67 = {
+	"id": "67",
+	"alias": "Mary_Louise",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_68 = {
+	"id": "68",
+	"alias": "Nancy",
+	"role": "3",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_69 = {
+	"id": "69",
+	"alias": "Paul_T",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_70 = {
+	"id": "70",
+	"alias": "James_K",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_71 = {
+	"id": "71",
+	"alias": "William_S",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_72 = {
+	"id": "72",
+	"alias": "Michael_F",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_73 = {
+	"id": "73",
+	"alias": "Matthew_J",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_74 = {
+	"id": "74",
+	"alias": "Benjamin_L",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_75 = {
+	"id": "75",
+	"alias": "Anna_S",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_76 = {
+	"id": "76",
+	"alias": "Sarah",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_77 = {
+	"id": "77",
+	"alias": "Emma_W",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_78 = {
+	"id": "78",
+	"alias": "Charlotte",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_79 = {
+	"id": "79",
+	"alias": "Grace_H",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_80 = {
+	"id": "80",
+	"alias": "Victoria_V",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_81 = {
+	"id": "81",
+	"alias": "Abigail",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_82 = {
+	"id": "82",
+	"alias": "Hannah",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_83 = {
+	"id": "83",
+	"alias": "Sophia_G",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_84 = {
+	"id": "84",
+	"alias": "Luke_W",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_85 = {
+	"id": "85",
+	"alias": "Jason_M",
+	"role": "1",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_86 = {
+	"id": "86",
+	"alias": "Nicholas_F",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_87 = {
+	"id": "87",
+	"alias": "Ethan_H",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_88 = {
+	"id": "88",
+	"alias": "Zachary_B",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_89 = {
+	"id": "89",
+	"alias": "Jonathan",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
+worker_90 = {
+	"id": "90",
+	"alias": "Logan_P",
+	"role": "0",
+	"tipo": "0",
+	"state": "0",
+	"formacion": [],
+	"horas": 0
+	}
