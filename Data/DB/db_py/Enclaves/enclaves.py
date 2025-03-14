2f6d747eb66d0bcd8b9aa13f8c2e5ed956d4fe27
Nihilus Null Zero <135299351+n1h1lius@users.noreply.github.com>
2025-03-14 04:07:34 +0100

    Add files via upload

---
+++ Data/DB/db_py/Enclaves/enclaves.py
@@ -0,0 +1,253 @@
+
+enclave_0 = {
+	"id": "0",
+	"alias": "NovaSphere",
+	"demanda_h": 5.0,
+	"ratio": 4.0
+	}
+enclave_1 = {
+	"id": "1",
+	"alias": "Zenith_Dynamics",
+	"demanda_h": 5.0,
+	"ratio": 4.0
+	}
+enclave_2 = {
+	"id": "2",
+	"alias": "StellarEdge",
+	"demanda_h": 5.0,
+	"ratio": 0.0
+	}
+enclave_3 = {
+	"id": "3",
+	"alias": "CrimsonPeak",
+	"demanda_h": 5.0,
+	"ratio": 0.0
+	}
+enclave_4 = {
+	"id": "4",
+	"alias": "QuantumStream",
+	"demanda_h": 5.0,
+	"ratio": 0.0
+	}
+enclave_5 = {
+	"id": "5",
+	"alias": "ApexHorizon_Corp",
+	"demanda_h": 7.0,
+	"ratio": 0.0
+	}
+enclave_6 = {
+	"id": "6",
+	"alias": "Silverline",
+	"demanda_h": 9.0,
+	"ratio": 0.0
+	}
+enclave_7 = {
+	"id": "7",
+	"alias": "Emberglow",
+	"demanda_h": 7.5,
+	"ratio": 3.0
+	}
+enclave_8 = {
+	"id": "8",
+	"alias": "TitanForge",
+	"demanda_h": 7.5,
+	"ratio": 0.0
+	}
+enclave_9 = {
+	"id": "9",
+	"alias": "BlueCascade",
+	"demanda_h": 7.5,
+	"ratio": 3.0
+	}
+enclave_10 = {
+	"id": "10",
+	"alias": "RadiantTrail",
+	"demanda_h": 7.0,
+	"ratio": 0.0
+	}
+enclave_11 = {
+	"id": "11",
+	"alias": "OrbitEra",
+	"demanda_h": 7.0,
+	"ratio": 0.0
+	}
+enclave_12 = {
+	"id": "12",
+	"alias": "SwiftPine_Logistics",
+	"demanda_h": 7.0,
+	"ratio": 0.0
+	}
+enclave_13 = {
+	"id": "13",
+	"alias": "IronCrest_Group",
+	"demanda_h": 7.0,
+	"ratio": 0.0
+	}
+enclave_14 = {
+	"id": "14",
+	"alias": "Ecolume_Systems",
+	"demanda_h": 7.0,
+	"ratio": 0.0
+	}
+enclave_15 = {
+	"id": "15",
+	"alias": "OmniWave_Holdings",
+	"demanda_h": 7.25,
+	"ratio": 4.0
+	}
+enclave_16 = {
+	"id": "16",
+	"alias": "LunarPath_Entreprises",
+	"demanda_h": 7.25,
+	"ratio": 4.0
+	}
+enclave_17 = {
+	"id": "17",
+	"alias": "BoldCove",
+	"demanda_h": 7.5,
+	"ratio": 8.0
+	}
+enclave_18 = {
+	"id": "18",
+	"alias": "MiragePoint",
+	"demanda_h": 7.5,
+	"ratio": 5.0
+	}
+enclave_19 = {
+	"id": "19",
+	"alias": "InfiniteBloom",
+	"demanda_h": 7.5,
+	"ratio": 5.0
+	}
+enclave_20 = {
+	"id": "20",
+	"alias": "PrismArc",
+	"demanda_h": 8.0,
+	"ratio": 5.0
+	}
+enclave_21 = {
+	"id": "21",
+	"alias": "VentureVista",
+	"demanda_h": 8.0,
+	"ratio": 5.0
+	}
+enclave_22 = {
+	"id": "22",
+	"alias": "SolarFlare",
+	"demanda_h": 7.5,
+	"ratio": 9.0
+	}
+enclave_23 = {
+	"id": "23",
+	"alias": "UrbanPeak",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_24 = {
+	"id": "24",
+	"alias": "ThunderGate",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_25 = {
+	"id": "25",
+	"alias": "NexusForge",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_26 = {
+	"id": "26",
+	"alias": "ClearStream",
+	"demanda_h": 4.0,
+	"ratio": 1.0
+	}
+enclave_27 = {
+	"id": "27",
+	"alias": "ElectroLoom",
+	"demanda_h": 7.5,
+	"ratio": 3.0
+	}
+enclave_28 = {
+	"id": "28",
+	"alias": "FireHaven",
+	"demanda_h": 7.5,
+	"ratio": 3.0
+	}
+enclave_29 = {
+	"id": "29",
+	"alias": "AquaNova",
+	"demanda_h": 7.5,
+	"ratio": 6.0
+	}
+enclave_30 = {
+	"id": "30",
+	"alias": "EmeraldSky",
+	"demanda_h": 7.5,
+	"ratio": 0.0
+	}
+enclave_31 = {
+	"id": "31",
+	"alias": "StratusEdge",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_32 = {
+	"id": "32",
+	"alias": "TrailBlaze",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_33 = {
+	"id": "33",
+	"alias": "VelocityWave",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_34 = {
+	"id": "34",
+	"alias": "AmberLight",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_35 = {
+	"id": "35",
+	"alias": "ZenithPinnacle_Corp",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_36 = {
+	"id": "36",
+	"alias": "HorizonDrift",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_37 = {
+	"id": "37",
+	"alias": "SolarCrest_Systems",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_38 = {
+	"id": "38",
+	"alias": "CloudStream_Enterprises",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_39 = {
+	"id": "39",
+	"alias": "CrimsonTrail_Solutions",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_40 = {
+	"id": "40",
+	"alias": "NeoGlow_Ventures",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
+enclave_41 = {
+	"id": "41",
+	"alias": "Tech_Systems",
+	"demanda_h": 7.5,
+	"ratio": 4.0
+	}
